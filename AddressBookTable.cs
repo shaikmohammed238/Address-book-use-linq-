82a489ae7b86817d11a2a6734533bb2bf5e07839
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-02-06 21:10:03 +0530

    added uc1

---
+++ AddressBookTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem_linq_
+{
+    class AddressBookTable
+    {
+        DataTable Table = new DataTable("AddressBookSystem");   // create a table
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿ using System;
+
+namespace AddressBookSystem_linq_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
